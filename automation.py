b0a8e408a73d758cb134206965e586cc5e494126
Pedro <xibiuveio1234@gmail.com>
2026-05-14 12:27:19 -0300

    First commit

---
+++ automation.py
@@ -0,0 +1,27 @@
+import subprocess
+from time import sleep
+from tkinter import filedialog
+
+import customtkinter as ctk
+import pyautogui
+
+from interface import achar_arquivo, main
+
+pyautogui.PAUSE = 0.3
+arquivo = main()
+if arquivo:
+    arquivo = arquivo.replace("/", "\\")
+    print(arquivo)
+    subprocess.run(["powershell", "-command", f"Set-Clipboard -Path '{arquivo}'"])
+    subprocess.Popen(
+        [r"C:\Program Files\Google\Chrome\Application\chrome.exe", "https://claude.ai"],
+        creationflags=subprocess.CREATE_NO_WINDOW,
+    )
+    sleep(2)
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.write(
+        "Mande uma versao corrigida (esteticamente, se necessario; gramaticalmente, se tiver erros e repeticoes ou se haver algo que tem como melhorar; e se for uma prova ou algo do tipo, melhore o jeito em que as questões sao feitas, como: se tiverem 7 questões pra 1 ponto, melhore, pois nao há um jeito certo de dividir corretamente) a questao"
+    )
+    sleep(3.5)
+    print("Esperei")
+    pyautogui.press("enter")
---
+++ interface.py
@@ -0,0 +1,34 @@
+from tkinter import filedialog
+
+import customtkinter as ctk
+
+caminho = None
+
+
+def achar_arquivo():
+    global caminho
+    caminho = filedialog.askopenfilename(
+        title="Escolha um arquivo Word", filetypes=[("Arqivos Word", "*.docx")]
+    )
+    return caminho if caminho and caminho.endswith(".pdf") else None
+
+
+def main():
+    ctk.set_default_color_theme("blue")
+    ctk.set_appearance_mode("dark")
+    app = ctk.CTk()
+    app.geometry("500x500")
+    app.iconbitmap(r"C:\Users\Home\Downloads\document-project\logo.ico")
+    app.title("Corretor de arquivos Word")
+    button = ctk.CTkButton(app, text="Escolha arquivo", command=achar_arquivo)
+    text = ctk.CTkLabel(
+        app, text="Clique no botão abaixo para escolher um arquivo e saia da janela."
+    )
+    text.place(relx=0.5, rely=0.25, anchor="center")
+    button.place(relx=0.5, rely=0.5, anchor="center")
+    app.mainloop()
+    return caminho
+
+
+if __name__ == "__main__":
+    main()
